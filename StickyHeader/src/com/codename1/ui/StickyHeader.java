323229689f30d876135c40b96fe7b70cf39b28ed
Chen Fishbein <cf27675@gmail.com>
2016-05-22 17:08:55 +0300

    changed Stack to ArrayList

--- StickyHeader/src/com/codename1/ui/StickyHeader.java
+++ StickyHeader/src/com/codename1/ui/StickyHeader.java
@@ -10,7 +10,7 @@ import com.codename1.ui.Graphics;
 import com.codename1.ui.Painter;
 import com.codename1.ui.events.ScrollListener;
 import com.codename1.ui.geom.Rectangle;
-import java.util.Stack;
+import java.util.ArrayList;
 
 /**
  *
@@ -59,8 +59,8 @@ public class StickyHeader extends Container implements ScrollListener {
                 pushToHeader();
             }
         }else{
-            Stack stack = (Stack) getParent().getClientProperty("sticky");
-            if (stack != null && !stack.empty() && stack.peek() == this && position<0) {
+            ArrayList stack = (ArrayList) getParent().getClientProperty("sticky");
+            if (stack != null && !stack.isEmpty()&& stack.get(0) == this && position<0) {
                 popFromHeader();
             }
         }
@@ -72,11 +72,11 @@ public class StickyHeader extends Container implements ScrollListener {
     }
 
     private void popFromHeader() {
-        Stack stack = (Stack) getParent().getClientProperty("sticky");        
-        stack.pop();
+        ArrayList stack = (ArrayList) getParent().getClientProperty("sticky");        
+        stack.remove(0);
         
-        if(!stack.empty()){
-            StickyHeader h = (StickyHeader)stack.peek();
+        if(!stack.isEmpty()){
+            StickyHeader h = (StickyHeader)stack.get(0);
             h.installSticky();
         }else{
             getComponentForm().setGlassPane(null);
@@ -84,18 +84,18 @@ public class StickyHeader extends Container implements ScrollListener {
     }
     
     private void pushToHeader() {
-        Stack stack = (Stack) getParent().getClientProperty("sticky");        
+        ArrayList stack = (ArrayList) getParent().getClientProperty("sticky");        
         if (stack == null) {            
-            stack = new Stack();
+            stack = new ArrayList();
             getParent().putClientProperty("sticky", stack);
         }
-        if(!stack.empty()){
-            StickyHeader h = (StickyHeader) stack.peek();
+        if(!stack.isEmpty()){
+            StickyHeader h = (StickyHeader) stack.get(0);
             if(getY() < h.getY()){
                 return;
             }
         }
-        stack.push(this);
+        stack.add(0, this);
         installSticky();
     }
 
